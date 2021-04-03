95036c78739f37cd39f1d47a13deafe1f3b4dcd8
sal90oo <fifa160016@gmail.com>
2021-04-03 12:41:00 +0300

    update database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,29 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
