5e06c81fbf9ffc13f207d3e1cefd9c0a00c2b93b
sal90oo <fifa160016@gmail.com>
2021-04-03 13:00:08 +0300

    update database

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -20,11 +20,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
-      options: {
-        ssl: true,
-      },
+      options: {},
     },
   },
 });
